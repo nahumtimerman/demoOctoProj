b141f645aa064c53e21f14b652efab983105f8bd
Nahum Timerman <ntimerman@gmail.com>
2017-01-15 13:44:22 +0200

    octotest

---
+++ OctoTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OctoTest.Startup))]
+namespace OctoTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
