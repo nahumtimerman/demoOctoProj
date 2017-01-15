b6cb7f0b70aa1f5cc5b88a97caf35315daf4c816
Nahum Timerman <ntimerman@gmail.com>
2017-01-15 12:05:48 +0200

    tc octo new

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tc_and_octo.Startup))]
+namespace tc_and_octo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
